e9e7d4d07271dcd72fa8bc7514b85a3459a83704
Pinyada Sengate <pinyada.sengate@gmail.com>
2025-04-06 11:10:38 +0200

    use dns for pod to pod communication

--- kube-network/users-api/users-app.js
+++ kube-network/users-api/users-app.js
@@ -24,7 +24,7 @@ app.post("/signup", async (req, res) => {
 
   try {
     const hashedPW = await axios.get(
-      `http://${process.env.AUTH_ADDRESS}/hashed-password/${password}`
+      `http://${process.env.AUTH_SERVICE_SERVICE_HOST}/hashed-password/${password}`
     );
     // since it's a dummy service, we don't really care for the hashed-pw either
     console.log(hashedPW, email);
@@ -56,7 +56,7 @@ app.post("/login", async (req, res) => {
   // normally, we'd find a user by email and grab his/ her ID and hashed password
   const hashedPassword = password + "_hash";
   const response = await axios.get(
-    `http://${process.env.AUTH_ADDRESS}/token/${hashedPassword}/${password}`
+    `http://${process.env.AUTH_SERVICE_SERVICE_HOST}/token/${hashedPassword}/${password}`
   );
   if (response.status === 200) {
     return res.status(200).json({ token: response.data.token });
